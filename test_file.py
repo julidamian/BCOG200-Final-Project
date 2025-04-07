fa791b462eadc149e89fe425ab93db19c3eca1e2
julidamian <143122847+julidamian@users.noreply.github.com>
2025-04-07 15:04:15 -0500

    Add files via upload

---
+++ test_file.py
@@ -0,0 +1,14 @@
+"""
+If I am understanding Test File correctly, I would think that in order for the user to test
+functionality, they would be required to upload a photo to see if that part of my program works.
+
+For the test, I would probably have the user upload with one specific image first, just to get the 
+foundation of the code working. This would be a real image taken by me , most likely.
+
+After that, I could expand to using any image the user wants.
+
+To conduct the test for the coloring, I would just use any image I chose to upload to ensure the 
+coloring function worked.
+
+Afterwards, I would check to ensure coloring the image the user uploaded works. 
+"""
